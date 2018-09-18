bc8259d093d073b821523c3fc15034a138e48b95
BT_1N3_17@bktech.local <BT_1N3_17@bktech.local>
2018-09-18 09:17:06 -0400

    Initial commit

---
+++ src/JLin/JLinLib.java
@@ -0,0 +1,9 @@
+package JLin;
+
+public class JLinLib {
+
+    public static void println(String str)
+    {
+        System.out.println(str);
+    }
+}
---
+++ src/JLin/TestFile.java
@@ -0,0 +1,11 @@
+package JLin;
+
+import static JLin.JLinLib.println;
+
+public class TestFile {
+
+    public static void main(String[] args)
+    {
+        println("I love a cup of Java in the morning.");
+    }
+}
