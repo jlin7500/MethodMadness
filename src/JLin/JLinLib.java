d7cc3a920d149b35cade26cfb980a9d19ee30dc2
jlin7500 <jlin7500@bths.edu>
2018-09-18 10:13:48 -0400

    End of class update.

--- src/JLin/JLinLib.java
+++ src/JLin/JLinLib.java
@@ -6,4 +6,20 @@ public class JLinLib {
     {
         System.out.println(str);
     }
+    public static String isPalindrome(String str)
+        {
+            String x = new String("");
+            for(int i = str.length(); i > 0; i--)
+                {
+                    String y = str.substring(i,i-1
+                    );
+                    x = x + y;
+                    if(x.equals(y))
+                    {
+                        return "true";
+                    }
+                    return "false";
+                }
+                return "false";
+        }
 }
--- src/JLin/TestFile.java
+++ src/JLin/TestFile.java
@@ -1,5 +1,6 @@
 package JLin;
 
+import static JLin.JLinLib.isPalindrome;
 import static JLin.JLinLib.println;
 
 public class TestFile {
@@ -7,5 +8,7 @@ public class TestFile {
     public static void main(String[] args)
     {
         println("I love a cup of Java in the morning.");
+        println(isPalindrome("lmao"));
     }
+
 }
