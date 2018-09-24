d0f11e2d3b3a0b199f436ba434bbde783ae739e4
BT_1N3_17 <jlin7500@bths.edu>
2018-09-24 10:19:41 -0400

    method madness update

--- src/JLin/TestFile.java
+++ src/JLin/TestFile.java
@@ -8,13 +8,13 @@ public class TestFile {
     public static void main(String[] args)
     {
         println("I love a cup of Java in the morning.");
-        println(isPalindrome(""));
+        isPalindrome("dasdasdasdas");
         //this function is cut out what it does
         println(cutOut("ratcatbat","cat"));
         println(sumUpTo(10));
         println(dateStr("04/10/2001"));
         println(multiplicationTable(5,10));
-        println(isFibonnaci(5));
+        isFibonnaci(5);
     }
 
 }
